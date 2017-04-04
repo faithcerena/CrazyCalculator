bf57056f32742391052f7be6c0edba5a0d3af96e
Christopher Jan <christopherjan1999@gmail.com>
2017-04-04 23:23:06 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ public class Main{
   public static void main(String[] args){
     CalculatorFrame calculatorFrame = new CalculatorFrame();
     calculatorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    // calculatorFrame.setSize(356, 684);
     calculatorFrame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
     calculatorFrame.setUndecorated(true);
     calculatorFrame.setUndecorated(true);
